571e51b17ceb02a0450a16e1e76f1d7aa2c657f5
Serpiel <hmarujo1510@gmail.com>
2026-01-27 11:00:14 +0100

    Add controllers + update pages

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController extends AbstractController
+final class BlogController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/blog', name: 'app_blog')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
         ]);
-    } 
+    }
 }
